5d2e7a88faf6781900e4497b2462526c94cd8ac8
smit-sk <patelsmit7008@gmail.com>
2024-09-22 17:23:16 -0400

    "access allow to all"

--- app.js
+++ app.js
@@ -21,10 +21,10 @@ document
         "http://52.228.45.138:80/upload",
         formData,
         {
-          mode: "no-cors",
+          // mode: "no-cors",
           headers: {
             "Content-Type": "multipart/form-data",
-            // "Access-Allow-Origin": "*",
+            "Access-Allow-Origin": "*",
           },
         }
       );
