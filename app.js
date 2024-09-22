b495f0a6d25595bb4ad8be866f1c95d11b14344d
smit-sk <patelsmit7008@gmail.com>
2024-09-22 17:12:03 -0400

    "added no mode"

--- app.js
+++ app.js
@@ -21,8 +21,10 @@ document
         "http://52.228.45.138:80/upload",
         formData,
         {
+          mode: "no-cors",
           headers: {
             "Content-Type": "multipart/form-data",
+            // "Access-Allow-Origin": "*",
           },
         }
       );
